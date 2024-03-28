c3ad98bfa75c0c5fe4683e66c0a2f36aa55cdae6
Matt Townsend <mt76@gmx.com>
2024-03-28 08:41:19 +0000

    Add empty functions for display package

--- display/display.go
+++ display/display.go
@@ -1,1 +1,34 @@
 package display
+
+import "fmt"
+
+// gtrm.MoveCursor(startColumn, 21)
+// gtrm.Print(t.row())
+// gtrm.Println(p.FormatRowOutput(p.pageRows[i].RowContent))
+// gtrm.Flush()
+// gtrm.Clear()
+
+func PrintAt(content string, x, y int) {
+	fmt.Print(content)
+}
+
+func Flush() {
+	// Do nothing
+}
+
+func Clear() {
+	// Do nothing
+}
+
+func MoveCursor(x, y int) {
+	// Do nothing
+}
+
+func Println(content string) {
+	// Do nothing
+	Print(content + "\n")
+}
+
+func Print(content string) {
+	// Do nothing
+}
